20db768155c6749a0b3fff25cfdcf203973f09ac
Antonio Hickey <antoniohickey99@gmail.com>
2023-01-04 20:53:40 -0500

    feat: ability to create symmetric key

--- src/generateKeys.ts
+++ src/generateKeys.ts
@@ -1,13 +1,41 @@
+import { generateKey, publicEncrypt } from "node:crypto";
 import inquirer from "inquirer";
-import { createSpinner } from 'nanospinner';
+import inquirerFileTreeSelection from 'inquirer-file-tree-selection-prompt';
 import fs from "fs";
+import { createSpinner } from 'nanospinner';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const myKeysPath = __dirname + '/../src/keys/my-keys/public';
+const _myKeysPath = myKeysPath.replace('dist/../', '') + '/';
+const peersKeysPath = __dirname + '/../src/keys/peers-pub-keys';
+const _peersKeysPath = peersKeysPath.replace('dist/../', '') + '/';
+const sharedKeysPath = __dirname + '/../src/keys/shared-priv-keys';
+const _sharedKeysPath = sharedKeysPath.replace('dist/../', '') + '/';
 
 
 export async function genNewKeyPair() {
-  // TODO: allow for the user to specify type
-  //       instead of hardcoding rsa.
-  let keyPairOptions = {}
+  await inquirer.prompt({
+    name: 'key_type',
+    type: 'list',
+    message: 'What Kind Of Key ?',
+    choices: [
+      'Asymmetric (RSA)',
+      'Symmetric (AES)',
+    ],
+    filter: (val: string) => val.toLowerCase(),
+  }).then(async (choice: object) => {
+    if (choice['key_type'] == "asymmetric (rsa)") {
+      generateRsaKeys();
+    } else {
+      generateAesKey();
+    };
+  });
+}
 
+async function generateRsaKeys() {
   await inquirer.prompt([
     {
       name: 'key_pair_name',
@@ -32,7 +60,7 @@ export async function genNewKeyPair() {
       filter(val: string) {
         return +val.split(" bits")[0];  // Parsed to number
       },
-    }
+    },
   ]).then(async (keyPairOpts: object) => {
     const spinner = createSpinner('Creating keys').start();
     const { generateKeyPair } = await import('node:crypto');
@@ -58,7 +86,7 @@ export async function genNewKeyPair() {
       let keyPair = [
         { filename: filename.replace('{PUB/PRIV}', 'private'), content: privKey.toString() },
         { filename: filename.replace('{PUB/PRIV}', 'public'), content: pubKey.toString() },
-      ]
+      ];
 
       for (let key of keyPair) {
         fs.writeFile(key.filename, key.content, (err) => { if (err) throw err });
@@ -67,6 +95,51 @@ export async function genNewKeyPair() {
 
     spinner.success();
   });
+}
 
-  return keyPairOptions;
+async function generateAesKey() {
+  await inquirer.prompt([
+    {
+      name: 'key_pair_name',
+      type: 'input',
+      message: 'Key Pair Name:',
+    },
+    {
+      name: 'for_who',
+      type: 'list',
+      message: 'Who Is The Key For:',
+      choices: ['Myself', 'Someone Else'],
+      filter: (val: string) => val.toLowerCase(),
+    },
+  ]).then(async (choice: object) => {
+    await inquirer.prompt(choice['for_who'] == 'myself' ? {
+      name: 'key_to_use',
+      type: 'file-tree-selection',
+      message: 'Select Which Key To Encrypt With:',
+      root: myKeysPath,
+      transformer: (input: string) => input.replace(_myKeysPath, ''),
+    } : {
+      name: 'key_to_use',
+      type: 'file-tree-selection',
+      message: 'Select Which Key To Encrypt With:',
+      root: peersKeysPath,
+      transformer: (input: string) => input.replace(_peersKeysPath, ''),  
+    }).then((choiceTwo: object) => {
+      const spinner = createSpinner('Creating Key').start();
+      let filePath = _sharedKeysPath + choice['key_pair_name'] + ".encrypted";
+      let aesKey: string;
+
+      generateKey('aes', { length: 256 }, (err, key) => {
+        if (err) throw err;
+        aesKey = key.export().toString('hex');
+      });
+
+      let pubKey = fs.readFileSync(choiceTwo['key_to_use'], 'utf8');
+      let encryptedAesKey = publicEncrypt(pubKey, Buffer.from(aesKey, 'hex'));
+
+      fs.writeFileSync(filePath, encryptedAesKey.toString('hex'));
+
+      spinner.success({ text: 'Created Key' });
+    });
+  });
 }
