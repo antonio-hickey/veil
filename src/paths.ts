88b5842d36520d6e69925fd7c19f0030df84dc9b
Antonio Hickey <antoniohickey99@gmail.com>
2023-01-07 18:26:00 -0500

    refactor: use func to reduce code duplication

--- src/paths.ts
+++ src/paths.ts
@@ -4,14 +4,16 @@ import { dirname } from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const myPubKeysPath = __dirname + '/../src/keys/my-keys/public';
-const _myPubKeysPath = myPubKeysPath.replace('dist/../', '') + '/';
 const myPrivKeysPath = __dirname + '/../src/keys/my-keys/private';
-const _myPrivKeysPath = myPrivKeysPath.replace('dist/../', '') + '/';
 const peersKeysPath = __dirname + '/../src/keys/peers-pub-keys';
-const _peersKeysPath = peersKeysPath.replace('dist/../', '') + '/';
 
 
+async function getRealPath(path: string): Promise<string> {
+	return path.replace('dist/../', '') + '/';
+}
+
+ 
 export {
-	__filename, __dirname, myPubKeysPath, _myPubKeysPath,
-	myPrivKeysPath, _myPrivKeysPath, peersKeysPath, _peersKeysPath,
+	__filename, __dirname, myPubKeysPath,
+	myPrivKeysPath, peersKeysPath, getRealPath,
 };
